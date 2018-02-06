a09471be24c3c8f4fe2a919feebc1336d469b0b1
batuZ <304701204@qq.com>
2018-02-06 18:14:13 +0800

    add thumbViewer

---
+++ ImageViewerBase/ThumbText/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ThumbText
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(42, 43);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(200, 100);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(782, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(35, 20);
+            this.toolStripMenuItem1.Text = "123";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 597);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ ImageViewerBase/ThumbText/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThumbText
+{
+    public partial class Form1 : Form
+    {
+        int ii;
+        int picBoxSize = 128;
+        int ThumbSize = 128;
+        List<string> photos = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                photos = openFileDialog1.FileNames.ToList();
+                UpdateItems();
+            }
+        }
+
+        #region PicBoxItems
+        void UpdateItems()
+        {
+            flowLayoutPanel1.Controls.Clear();
+            creatPicBoxes();
+            upDataThrmMaps();
+        }
+
+        void creatPicBoxes()
+        {
+            PictureBox abox = new PictureBox()
+            {
+                BackColor = SystemColors.ScrollBar,
+                BackgroundImageLayout = ImageLayout.Zoom,
+                SizeMode = PictureBoxSizeMode.Zoom,
+                Margin = new Padding(2),
+                Size = new Size(picBoxSize, picBoxSize)
+            };
+            abox.MouseClick += Abox_MouseClick;
+            abox.MouseDoubleClick += Abox_MouseDoubleClick;
+            for (int i = 0; i < photos.Count; i++)
+            {
+                string photoPath = photos[i];
+                //如果已存在，则返回此实例
+                if (flowLayoutPanel1.Controls.ContainsKey(photoPath))
+                    abox = flowLayoutPanel1.Controls[photoPath] as PictureBox;
+                else
+                    abox.Name = photoPath;
+                flowLayoutPanel1.Controls.Add(abox);
+            }
+        }
+        /**
+                 void creatPicBoxes()
+        {
+            PictureBox abox = null;
+            for (int i = 0; i < photos.Count; i++)
+            {
+                string photoPath = photos[i];
+                //如果已存在，则返回此实例
+                if (flowLayoutPanel1.Controls.ContainsKey(photoPath))
+                    abox = flowLayoutPanel1.Controls[photoPath] as PictureBox;
+                else
+                {
+                    abox = new PictureBox()
+                    {
+                        BackColor = SystemColors.ScrollBar,
+                        BackgroundImageLayout = ImageLayout.Zoom,
+                        SizeMode = PictureBoxSizeMode.Zoom,
+                        Margin = new Padding(2),
+                        Name = photoPath,
+                        Size = new Size(picBoxSize, picBoxSize)
+                    };
+                    abox.MouseClick += Abox_MouseClick;
+                    abox.MouseDoubleClick += Abox_MouseDoubleClick;
+                }
+                flowLayoutPanel1.Controls.Add(abox);
+            }
+        }
+             */
+        private void Abox_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Abox_MouseClick(object sender, MouseEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        
+        #region ThumbMap
+        Thread getThumbMap_SubThread;
+        void upDataThrmMaps()
+        {
+            if (getThumbMap_SubThread == null || getThumbMap_SubThread.ThreadState == ThreadState.Stopped)
+            {
+                getThumbMap_SubThread = new Thread(new ThreadStart(() => getThumbMapFromImage(callback_setToPic)));
+                getThumbMap_SubThread.Start();
+                MessageBox.Show("开始提取缩略图");
+            }
+            else
+                MessageBox.Show("正在提取缩略图");
+        }
+        void getThumbMapFromImage(Func<string, string, bool> setToPic)
+        {
+            for (ii = 0; ii < photos.Count; ii++)
+            {
+                int i = ii;//界面点击会改变此值
+                string imgPaht = photos[i];
+                string thumbPath = getThumbPath(imgPaht, "");
+                //如果thumbPath不存在则创建缩略图
+                if (!File.Exists(thumbPath))
+                {
+                    Image aMap = Image.FromFile(imgPaht);
+                    //缩略图尺寸
+                    int thuWidth = aMap.Width > aMap.Height ? ThumbSize : ThumbSize * aMap.Height / aMap.Width;
+                    int thuHieght = aMap.Width > aMap.Height ? ThumbSize * aMap.Height / aMap.Width : ThumbSize;
+                    //创建缩略图
+                    Image thumbMap = aMap.GetThumbnailImage(thuWidth, thuHieght, null, new IntPtr());
+                    //创建文件夹，保存缩略图
+                    if (Directory.Exists(Path.GetDirectoryName(thumbPath)))
+                        Directory.CreateDirectory(Path.GetDirectoryName(thumbPath));
+                    try { thumbMap.Save(thumbPath, ImageFormat.Jpeg); } catch (Exception ex) { MessageBox.Show(ex.Message); };
+                    aMap.Dispose();
+                    thumbMap.Dispose();
+                }
+                //回调返回false,说明主线程断了，则break掉此线程的循环
+                if (!setToPic(imgPaht, thumbPath)) break;
+            }
+        }
+        bool callback_setToPic(string imgPath, string ThumbPath)
+        {
+            bool res = false;
+            try
+            {
+                (flowLayoutPanel1.Controls[imgPath] as PictureBox).Image = Image.FromFile(ThumbPath);
+                res = true;
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+            return res;
+        }
+        string getThumbPath(string imgPaht, string ProName)
+        {
+            string imgName = Path.GetFileNameWithoutExtension(imgPaht) + ".jpg";
+            string dir = Path.GetFileName(Path.GetDirectoryName(imgPaht));
+            string appPath = Application.LocalUserAppDataPath;
+            string res = $"{appPath}\\Thumbnail\\{ProName}\\{dir}\\{imgName}";
+            return res;
+        }
+        #endregion
+    }
+}
