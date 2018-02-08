96a3ac09e4420cc784bf694e091381fcaa9d3550
batuZ <304701204@qq.com>
2018-02-08 23:26:52 +0800

    窗体停靠库

---
+++ DockPanel_WFL/README.md
@@ -0,0 +1,37 @@
+﻿1.说明
+
+weiFenLuo.winFormsUI.Docking.dll是开源项目DockPanel Suite的一个类库，可以实现像Visual Studio的窗口停靠、拖拽等功能。WeifenLuo.WinFormsUI.Docking是一个很强大的界面布局控件,可以保存自定义的布局为XML文件,可以加载XML配置文件。
+
+该动态库源自开源项目DockPanelSuite，原始下载链接如下：
+
+http://sourceforge.net/projects/dockpanelsuite/files/?source=navbar
+
+但目前，该动态库已经改至以下链接进行下载与维护：
+
+https://github.com/dockpanelsuite/dockpanelsuite
+
+可下载其源码、可直接使用的dll及示例程序，目前已更新至2.8，可在VS2012环境下使用。
+
+2.使用
+
+dockpanel中提供了几个可用的类, 重要的有两个, 一是DockPanel, 一是DockContent：
+
+DockPanel是从panel继承出来的, 用于提供可浮动的dock的子窗口进行浮动和dock的场所,
+DockContent是从form类中继承出来的, 用于提供可浮动的窗口基类. 就是说: DockContent对象可以在DockPanel对象中任意贴边, 浮动, TAB化等.  
+
+  添加引用：
+     1）引用—>添加引用—>浏览—>weiFenLuo.winFormsUI.Docking.dll。
+     2）窗体属性IsMdiContainer:True。
+     3）工具箱—>右键—>选择项—>.net组件—>浏览—>weiFenLuo.winFormsUI.Docking.dll—>在工具箱出现dockPanel。
+     4）将dockPanel拖到窗体Form1上，设置Dock属性为：Fill。
+  停靠窗体：
+     1.新建一个WinForm窗体Form2。
+     2.在代码中修改窗体继承于DockContent。
+     public partial class Form2 : DockContent
+     3.在主窗体Form1中显示停靠窗体。
+     private void Form1_Load(object sender, EventArgs e)
+     {
+　　     Form2 form2 = new Form2();
+　　     form2.Show(this.dockPanel1);
+　　     form2.DockTo(this.dockPanel1, DockStyle.Left);
+     }
---
+++ DockPanel_WFL/WeiFenLuo/DockFrom/MainForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace DockFrom
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //Buttom
+            DockForm buttom = new DockForm { BackColor = Color.Orange, Text = "bottom"};
+            buttom.Show(dockPanel1, DockState.DockBottom);
+            // 停靠窗占比：buttom,Hight / dockPanel.Hight
+            dockPanel1.DockBottomPortion = 0.3;
+
+            //Left
+            DockForm left = new DockForm() { BackColor = Color.Gray, Text = "left" };
+            left.Show(dockPanel1, DockState.DockLeft);
+            DockForm left_D = new DockForm() { BackColor = Color.Gold, Text = "left_D" };
+            // 子窗口各部占比 left_D / left
+            left_D.Show(left.Pane,DockAlignment.Bottom, 0.4);
+
+            //Right
+            DockForm right1 = new DockForm() { BackColor = Color.BurlyWood, Text = "right1" };
+            right1.Show(dockPanel1, DockState.DockRightAutoHide);
+            DockForm right2 = new DockForm() { BackColor = Color.DarkCyan, Text = "right2" };
+            //手动设置
+            right2.Show(dockPanel1);
+            right2.DockTo(dockPanel1, DockStyle.Right);
+            right2.DockState = DockState.DockRightAutoHide;
+
+            //Document
+            DockForm document = new DockForm() { BackColor = Color.DeepSkyBlue, Text = "Document" };
+            document.Show(dockPanel1, DockState.Document);
+
+            //SubDocker
+            DockForm subDocker_1 = new DockForm() { BackColor = Color.Green, Text = "Sub_Left" };
+            subDocker_1.Show(document.Pane, DockAlignment.Right, 0.5);
+            DockForm subDocker_2 = new DockForm() { BackColor = Color.PaleGreen, Text = "Sub_LeftDonw" };
+            subDocker_2.Show(document.Pane, DockAlignment.Bottom, 0.5);
+            DockForm subDocker_3 = new DockForm() { BackColor = Color.IndianRed, Text = "Sub_RightDonw" };
+            subDocker_3.Show(subDocker_1.Pane, DockAlignment.Bottom, 0.5);
+            
+            this.Text = dockPanel1.DockWindows.Count.ToString();
+        }
+    }
+}
