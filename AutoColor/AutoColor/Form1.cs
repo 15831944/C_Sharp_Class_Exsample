8e94e14532d2dd0ca765dc98cf7a6d9387d450f1
batuZ <304701204@qq.com>
2018-03-23 16:02:21 +0800

    getMinMax

--- AutoColor/AutoColor/Form1.cs
+++ AutoColor/AutoColor/Form1.cs
@@ -24,20 +24,56 @@ namespace AutoColor
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Dataset ds = Gdal.Open(@"E:\test\DSC00661.JPG", Access.GA_ReadOnly);
-            int bandCount = ds.RasterCount;
             int xSize = ds.RasterXSize;
             int ySize = ds.RasterYSize;
+            List<int[]> bufs = new List<int[]>();
+            for (int band = 0; band < ds.RasterCount; band++)
+            {
+                int[] buf = new int[xSize * ySize];
+                ds.GetRasterBand(band + 1).ReadRaster(0, 0, xSize, ySize, buf, xSize, ySize, 0, 0);
+                bufs.Add(buf);
+            }
+            numbers(bufs);
+            ds.Dispose();
 
-            Band band = ds.GetRasterBand(1);
-            DataType dt = band.DataType;
-            int[] buf = new int[xSize * ySize];
-            band.ReadRaster(0, 0, xSize, ySize, buf, xSize, ySize, 0, 0);
-            int min = buf.Min();
-            int max = buf.Max();
-            double Average = buf.Average();
-            Console.WriteLine("");
         }
-        public  double myBiaoZhunC(double[] suzu)
+
+        void numbers(List<int[]> bufs)
+        {
+            List<int[]> acters = new List<int[]>();
+            for (int band = 0; band < 3; band++)
+            {
+                int[] ac = new int[256];
+                int[] buf = bufs[band];
+                for (int valus = 0; valus < buf.Length; valus++)
+                    ac[buf[valus]]++;
+
+                int filterCount = buf.Length / 6000;
+                int minValue = 0;
+                int maxValue = 255;
+                for (int k = 0; k < ac.Length; k++)
+                {
+                    if (ac[k] > filterCount && ac[k] < ac[k + 1])
+                    {
+                        minValue = k;
+                        break;
+                    }
+                }
+                for (int k = ac.Length - 1; k >= 0; k--)
+                {
+                    if (ac[k] > filterCount && ac[k] < ac[k - 1])
+                    {
+                        maxValue = k;
+                        break;
+                    }
+                }
+            }
+        }
+
+
+
+
+        public double myBiaoZhunC(int[] suzu)
         {
             double pingjunzhi = suzu.Average();
             double temp = 0;
@@ -47,6 +83,7 @@ namespace AutoColor
             }
             return Math.Sqrt(temp / suzu.Length);
         }
+
     }
 }
 // http://blog.csdn.net/u010771437/article/details/45371141
