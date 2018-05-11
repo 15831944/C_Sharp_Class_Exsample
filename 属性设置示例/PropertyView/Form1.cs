6b95431e6367d360a48d0fa27ab2f785fe6bb0ec
batuZ <304701204@qq.com>
2018-05-11 17:37:54 +0800

    sdds

--- 属性设置示例/PropertyView/Form1.cs
+++ 属性设置示例/PropertyView/Form1.cs
@@ -36,75 +36,207 @@ namespace PropertyView
         private Size windowSize = new Size(100, 100);
         private Font windowFont = new Font("宋体", 9, FontStyle.Regular);
         private Color toolbarColor = SystemColors.Control;
-        [CategoryAttribute("文档设置"),
-        DefaultValueAttribute(true)]
+
+
+        [Category("文档设置"), DefaultValue(true)]
         public bool SaveOnClose
         {
             get { return saveOnClose; }
             set { saveOnClose = value; }
         }
-        [CategoryAttribute("文档设置")]
+
+        [Category("文档设置")]
         public Size WindowSize
         {
             get { return windowSize; }
             set { windowSize = value; }
         }
-        [CategoryAttribute("文档设置")]
+
+        [Category("文档设置")]
         public Font WindowFont
         {
             get { return windowFont; }
             set { windowFont = value; }
         }
-        [CategoryAttribute("全局设置")]
+
+        [Category("全局设置")]
         public Color ToolbarColor
         {
             get { return toolbarColor; }
             set { toolbarColor = value; }
         }
-        [CategoryAttribute("全局设置"),
-        ReadOnlyAttribute(true),
-        DefaultValueAttribute("欢迎使用应用程序！")]
+
+        [Category("全局设置"), ReadOnly(true), DefaultValue("欢迎使用应用程序！")]
         public string GreetingText
         {
             get { return greetingText; }
             set { greetingText = value; }
         }
-        [CategoryAttribute("全局设置"),
-        DefaultValueAttribute(4)]
+
+        [Category("全局设置"), DefaultValue(4)]
         public int ItemsInMRUList
         {
             get { return itemsInMRU; }
             set { itemsInMRU = value; }
         }
-        [DescriptionAttribute("以毫秒表示的文本重复率。"),
-        CategoryAttribute("全局设置"),
-        DefaultValueAttribute(10)]
+
+        [Description("以毫秒表示的文本重复率。"), Category("全局设置"), DefaultValue(10)]
         public int MaxRepeatRate
         {
             get { return maxRepeatRate; }
             set { maxRepeatRate = value; }
         }
-        [BrowsableAttribute(false),
-        DefaultValueAttribute(false)]
+
+        [Browsable(false), DefaultValue(false)]
         public bool SettingsChanged
         {
             get { return settingsChanged; }
             set { settingsChanged = value; }
         }
-        [CategoryAttribute("版本"),
-        DefaultValueAttribute("1.0"),
-        ReadOnlyAttribute(true)]
+
+        [Category("版本"), DefaultValue("1.0"), ReadOnly(true)]
         public string AppVersion
         {
             get { return appVersion; }
             set { appVersion = value; }
         }
-        [CategoryAttribute("拼写检查选项"),
-        DefaultValueAttribute("DefaultFileName")]
+
+        [Category("拼写检查选项"), DefaultValue("DefaultFileName")]
         public string SpellingOptions
         {
             get { return this.DefaultFileName; }
             set { this.DefaultFileName = value; }
+        }
+    }
+
+    public class AppSettings
+    {
+        private string extendDirection = "横向";
+        private string extendConfig = "横向";
+        private string dataGroup = "不分组";
+        private string dataCollection = "合计";
+        private string blankLine = "不补充";
+
+        [CategoryAttribute("扩展方向"),
+        DefaultValueAttribute("横向"),
+         TypeConverter(typeof(ExtendDirection))]
+        public string 扩展方向
+        {
+            get { return extendDirection; }
+            set { extendDirection = value; }
+        }
+
+        [CategoryAttribute("可扩展性"),
+         DefaultValueAttribute("横向"),
+         TypeConverter(typeof(ExtendConfig))]
+        public string 可扩展性
+        {
+            get { return extendConfig; }
+            set { extendConfig = value; }
+        }
+
+        [CategoryAttribute("数据分组设置"),
+         DefaultValueAttribute("不分组"),
+         TypeConverter(typeof(DataGroup))]
+        public string 分组
+        {
+            get { return dataGroup; }
+            set { dataGroup = value; }
+        }
+
+        [CategoryAttribute("数据分组设置"),
+         DefaultValueAttribute("合计"),
+         TypeConverter(typeof(DataCollection))]
+        public string 汇总
+        {
+            get { return dataCollection; }
+            set { dataCollection = value; }
+        }
+
+        [CategoryAttribute("补充空白行"),
+        DefaultValueAttribute("不补充"),
+        TypeConverter(typeof(BlankLine))]
+        public string 补充空白行
+        {
+            get { return blankLine; }
+            set { blankLine = value; }
+        }
+
+    }
+
+    /// <summary>  
+    /// 扩展方向配置  
+    /// </summary>  
+    public class ExtendDirection : StringConverter
+    {
+        public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
+        {
+            return true;
+        }
+        public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+        {
+            return new StandardValuesCollection(new string[] { "横向", "纵向" });
+        }
+    }
+
+    /// <summary>  
+    /// 可扩展性配置  
+    /// </summary>  
+    public class ExtendConfig : StringConverter
+    {
+        public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
+        {
+            return true;
+        }
+        public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+        {
+            return new StandardValuesCollection(new string[] { "横向", "纵向" });
+        }
+    }
+
+    /// <summary>  
+    /// 数据分组设置  
+    /// </summary>  
+    public class DataGroup : StringConverter
+    {
+        public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
+        {
+            return true;
+        }
+        public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+        {
+            return new StandardValuesCollection(new string[] { "不分组", "相同分组" });
+        }
+    }
+
+    /// <summary>  
+    /// 数据汇总设置  
+    /// </summary>  
+    public class DataCollection : StringConverter
+    {
+        public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
+        {
+            return true;
+        }
+        public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+        {
+            return new StandardValuesCollection(new string[] { "合计", "平均", "最大", "最小", "记录数" });
+        }
+    }
+
+
+    /// <summary>  
+    /// 补充空白行  
+    /// </summary>  
+    public class BlankLine : StringConverter
+    {
+        public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
+        {
+            return true;
+        }
+        public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+        {
+            return new StandardValuesCollection(new string[] { "不补充", "补满页", "固定行" });
         }
     }
 }
